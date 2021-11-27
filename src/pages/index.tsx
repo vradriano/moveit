eeef2961a51cc34e0ace4273320c19ca5d676a51
Vitor Adriano <vradriano@hotmail.com>
2021-11-26 23:17:01 -0300

    creating countdown and button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,26 @@
 import { CompletedChallenge } from '../components/CompletedChallenge'
+import { Countdown } from '../components/Countdown'
 import { ExperienceBar } from '../components/ExperienceBar'
 import Profile from '../components/Perfil'
 
+import Head from 'next/head';
+
 import styles from '../styles/pages/Home.module.css'
+
 
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Move IT!</title>
+      </Head>
       <ExperienceBar />
-
-
       <section>
         <div>
           <Profile />
           <CompletedChallenge />
+          <Countdown />
         </div>
         <div>
 
